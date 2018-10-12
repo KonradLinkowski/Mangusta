da1c883bbe1fd1768759ee59baf60286aa54d97a
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-10-12 22:28:05 +0200

    Added mongoose dependency, added database connection

--- Back/src/index.js
+++ Back/src/index.js
@@ -4,10 +4,23 @@ const path = require('path')
 process.env.NODE_CONFIG_DIR = path.join(__dirname, 'config')
 const config = require('config')
 
+const mongoose = require('mongoose')
+
 const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
+const production = process.env.NODE_ENV === 'production'
+
+// set mongoose promise handling
+mongoose.Promise = global.Promise
+
+// connect to the database
+mongoose.connect(
+  production ? config.get('database.prod') : config.get('database.dev'),
+  { useNewUrlParser: true }
+)
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`)
