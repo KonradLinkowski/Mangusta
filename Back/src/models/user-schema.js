5a5135696277cdcdc13c70a1150ed45330b01532
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-10-12 22:32:52 +0200

    Added user schema
    Implemented #1

---
+++ Back/src/models/user-schema.js
@@ -0,0 +1,57 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+const SALT_WORK_FACTOR = 10
+
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  firstName: String,
+  lastName: String,
+  email: { type: String, index: true, unique: true },
+  password: { type: String, required: true },
+  creation: { type: Date, default: Date.now() },
+  lastLogin: { type: Date },
+  role: { type: Number, default: 0 }
+})
+
+const hashPassword = function(next) {
+  // only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) {
+    return next()
+  }
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (saltError, salt) => {
+    if (saltError) {
+      return next(saltError)
+    }
+    // hash the password using new salt
+    bcrypt.hash(this.password, salt, (hashingError, hash) => {
+      if (hashingError) {
+        return next(hashingError)
+      }
+      // override the cleartext password with the hashed one
+      this.password = hash
+      next()
+    })
+  })
+}
+
+const comparePassword = function(candidatePassword, cb) {
+  console.log(candidatePassword, this.password)
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) {
+      cb(err)
+      return
+    }
+    cb(null, isMatch)
+  })
+}
+
+UserSchema.pre('save', hashPassword)
+
+UserSchema.methods.comparePassword = comparePassword
+
+const User = mongoose.model('user', UserSchema, 'users')
+
+module.exports = User
