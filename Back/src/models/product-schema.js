b40f91b6c19d13d47d4cfa57afc4c198bc9c68b0
Wiktor Bałaban <wiktorbalaban@gmail.com>
2018-10-22 11:32:48 +0200

    Dodanie wyszikiwania po nazwie

--- Back/src/models/product-schema.js
+++ Back/src/models/product-schema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  price: { type: Number, required: true },
   description: String,
   creation: { type: Date, default: Date.now() },
   quantity: { type: Number, required: true },
--- Back/src/routes/product-routes.js
+++ Back/src/routes/product-routes.js
@@ -4,8 +4,36 @@ const express = require('express')
 const router = express.Router()
 const Product = require('../models/product-schema')
 
-router.route('/product').get((req, res) => {
-  res.send('Hello World!')
+router.route('/product').get(async(req, res) => {
+  const search = req.query.search
+  const categories = req.query.categories
+  const priceMin = req.query.priceMin
+  const priceMax = req.query.priceMax
+  const sort = req.query.sort
+  const dir = req.query.dir
+
+  // !priceMin = 0
+  // !priceMax = Number.MAX_VALUE
+
+  for (let i = 0; i < 100; i++) {
+    const product = {
+      price: Math.random(),
+      name: 'Cool ironing tool',
+      description: 'Very suitable',
+      quantity: Math.random(),
+      category: ['agd', 'rtv', 'kitchen', 'ironing']
+    }
+    const schema = new Product(product)
+    try {
+      await schema.save()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  res.json(await Product.find({
+    name: { '$regex' : search, '$options' : 'i' }
+  }))
 })
 
 module.exports = router
