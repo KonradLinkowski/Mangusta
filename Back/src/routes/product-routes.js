c455ff82d9a7b9050eed61d7882e41699224820f
Wiktor Bałaban <wiktorbalaban@gmail.com>
2018-10-22 15:16:40 +0200

    dodanie przeszukiwania po kategorii i przedziale cenowym

--- Back/src/routes/product-routes.js
+++ Back/src/routes/product-routes.js
@@ -5,34 +5,33 @@ const router = express.Router()
 const Product = require('../models/product-schema')
 
 router.route('/product').get(async(req, res) => {
-  const search = req.query.search
-  const categories = req.query.categories
-  const priceMin = req.query.priceMin
-  const priceMax = req.query.priceMax
+  const search = req.query.search || ''
+  const categories = req.query.categories || ['']
+  const priceMin = req.query.priceMin || 0
+  const priceMax = req.query.priceMax || Number.MAX_VALUE
   const sort = req.query.sort
   const dir = req.query.dir
 
-  // !priceMin = 0
-  // !priceMax = Number.MAX_VALUE
-
-  for (let i = 0; i < 100; i++) {
-    const product = {
-      price: Math.random(),
-      name: 'Cool ironing tool',
-      description: 'Very suitable',
-      quantity: Math.random(),
-      category: ['agd', 'rtv', 'kitchen', 'ironing']
-    }
-    const schema = new Product(product)
-    try {
-      await schema.save()
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  // for (let i = 0; i < 100; i++) {
+  //   const product = {
+  //     price: i,
+  //     name: 'big old table',
+  //     description: 'Very old',
+  //     quantity: 100-i,
+  //     category: ['furniture', 'kitchen']
+  //   }
+  //   const schema = new Product(product)
+  //   try {
+  //     await schema.save()
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   res.json(await Product.find({
-    name: { '$regex' : search, '$options' : 'i' }
+    name: { '$regex' : search, '$options' : 'i' },
+    price: { '$gte' :  priceMin, '$lte' :  priceMax},
+    category: {'$in': ['furniture']}
   }))
 })
 
