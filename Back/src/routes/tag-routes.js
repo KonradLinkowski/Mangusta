ef837dcdb75a755682eed416761e215f79f71584
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-11-10 19:20:46 +0100

    Change tag route to rreturn list of tags

--- Back/src/routes/tag-routes.js
+++ Back/src/routes/tag-routes.js
@@ -5,20 +5,11 @@ const router = express.Router()
 const Tag = require('../models/tag-schema')
 
 router.route('/tag').get(async(req, res) => {
-  const search = req.query.search || ''
-  // const tag = {
-  //   name: 'tree'
-  // }
-  // const schema = new Tag(tag)
-  // try {
-  //   await schema.save()
-  // } catch (err) {
-  //   console.log(err)
-  // }
-
-  res.json(await Tag.find({
-      name: { '$regex': search, '$options': 'i' }
-  }))
+  try {
+    return res.status(200).json(await Tag.find())
+  } catch (err) {
+    return res.status(500).send('Internal server error')
+  }
 })
 
 module.exports = router
