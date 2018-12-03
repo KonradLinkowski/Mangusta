dcf719b89d8d8980fb0f71eba4a774bec3a8468c
Wiktor Bałaban <wiktorbalaban@gmail.com>
2018-12-03 14:08:57 +0100

    Cors działa

--- Back/src/index.js
+++ Back/src/index.js
@@ -9,12 +9,14 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const passport = require('passport')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use(passport.initialize())
 require('../config/passport')(passport)
