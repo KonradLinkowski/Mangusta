f0a48d243fe1f4d47ac50926b4cc07bc126ecc32
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-12-10 20:30:47 +0100

    Frontend product validation

--- Front/src/components/AddProduct/AddProduct.vue
+++ Front/src/components/AddProduct/AddProduct.vue
@@ -12,21 +12,39 @@ export default {
         tags: [],
         name: '',
         description: '',
-        price: 0,
-        quantity: 0
+        price: 0.01,
+        quantity: 1
       },
       tags: []
     }
   },
   methods: {
     async createProduct() {
-      try {
-        await createProduct(this.product)
-        this.$notify(productAddSuccess)
-      } catch (err) {
+      if (this.validateProduct(this.product)) {
+        try {
+          await createProduct(this.product)
+          this.$notify(productAddSuccess)
+        } catch (err) {
+          this.$notify(productAddError)
+        }
+      } else {
         this.$notify(productAddError)
       }
     },
+    validateProduct(prod) {
+      if (Number.isNaN(Number(prod.price))) {
+        return false
+      }
+      if (!prod.price.toString().split('.')[1].lenght === 2) {
+        return false
+      }
+      if (!Number.isInteger(prod.quantity)) {
+        return false
+      }
+      if (prod.name.lenght !== 0) {
+        
+      }
+    }
   },
   mounted() {
     getTags()
