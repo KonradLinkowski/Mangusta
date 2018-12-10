b1c3820cd9223ac78f95337578a9e9e41d316438
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-12-10 20:30:29 +0100

    Backend product validation

--- Back/src/models/product-schema.js
+++ Back/src/models/product-schema.js
@@ -3,10 +3,28 @@ const mongoose = require('mongoose')
 
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.01,
+    max: 10000,
+    validate: {
+      validator: price => price.toString().split('.')[1].length === 2,
+      message: props => `${props.value} is not a valid price!`
+    }
+  },
   description: String,
-  creation: { type: Date, default: Date.now() },
-  quantity: { type: Number, required: true },
+  creation: { type: Date, default: new Date().toISOString() },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10000,
+    validate: {
+      validator: Number.isInteger,
+      message: props => `${props.value} is not a valid quantity!`
+    }
+  },
   category: [String],
   isDeleted: { type: Boolean, default: false }
 })
