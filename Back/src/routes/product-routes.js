f038a2743ed7e11dd65d0df242149f1c071c5282
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-01-24 10:59:48 +0100

    Added user product routes

--- Back/src/routes/product-routes.js
+++ Back/src/routes/product-routes.js
@@ -2,6 +2,7 @@
 const router = require('express').Router()
 const Product = require('../models/product-schema')
 const Tags = require('../models/tag-schema')
+const mongoose = require('mongoose')
 
 router.route('/product').get(async(req, res) => {
     const search = req.query.search || ''
@@ -24,6 +25,39 @@ router.route('/product').get(async(req, res) => {
             price: { '$gte': priceMin, '$lte': priceMax },
         }).sort({ price: dir }))
     }
+})
+
+router.route('/user/products/:id').get(async(req, res) => {
+    const search = req.query.search || ''
+    // każdy produkt ma mieć domyślnie TAG product
+    //   dzięki temu można je wyszukiwa bez podania tagu
+    const category = req.query.category || null
+    const priceMin = req.query.priceMin || 0
+    const priceMax = req.query.priceMax || Number.MAX_SAFE_INTEGER
+    const dir = req.query.dir || 1
+    const owner = req.params.id
+    if (!mongoose.Types.ObjectId.isValid(owner)) {
+        return res.json({ msg: 'Invalid user id' })
+    }
+    try {
+        if (category) {
+            res.json(await Product.find({
+                name: { '$regex': search, '$options': 'i' },
+                price: { '$gte': priceMin, '$lte': priceMax },
+                category: { '$in': category },
+                owner
+            }).sort({ price: dir }))
+        } else {
+            res.json(await Product.find({
+                name: { '$regex': search, '$options': 'i' },
+                price: { '$gte': priceMin, '$lte': priceMax },
+                owner
+            }).sort({ price: dir }))
+        }
+    } catch(error) {
+        // console.error(error)
+        res.json({ error })
+    } 
 })
 
 router.route('/product/:id').get(async(req, res) => {
